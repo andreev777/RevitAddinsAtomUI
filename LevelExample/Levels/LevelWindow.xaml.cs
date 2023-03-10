fc1c56a13f7237fdc15bee2670e2a124666b6c1e
Alex <andreev17491@gmail.com>
2023-03-10 10:56:22 +0500

    Создание репозитория

---
+++ LevelExample/Levels/LevelWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RevitAddinsUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class LevelWindow : Window
+    {
+        public LevelWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region МЕТОДЫ ПЕРЕТАСКИВАНИЯ И ЗАКРЫТИЯ ОКНА
+        private void DragWithMouse(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    Top = 0;
+                    WindowState = WindowState.Normal;
+                }
+
+                DragMove();
+            }
+        }
+
+        private void CommandBinding_CanExecute_1(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+        #endregion
+    }
+}
